9e91ca8f20f0a38c1125a8f0198efabae9d99687
Wyatt Mosteller <wbmosteller@student.fullail.edu>
2022-03-27 19:05:23 -0400

    added 404 page and connected links

--- website/views.py
+++ website/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+def error404(request):
+    return render(request, '404.html', {})
