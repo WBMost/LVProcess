12cd904f4961a67fa05550e01af3d0c83cdcc49e
Wyatt Mosteller <wbmosteller@student.fullail.edu>
2022-04-24 22:58:12 -0400

    fixed up base.html and added individual titles per page

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = "home"),
-    path('index.html', views.index, name = "index"),
     path('contact.html',views.contact, name = "contact"),
     path('404.html', views.error404, name = "404"),
 ]
