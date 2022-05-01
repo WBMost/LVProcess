75a241442a29ec9ef95dfac9c147f5300aa90056
Wyatt Mosteller <wbmosteller@student.fullail.edu>
2022-04-30 20:05:30 -0400

    turned debug on

--- lvprocess/settings.py
+++ lvprocess/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nj$%pe0qoxltm34k_jk_qfn-ym7h5kkeuud*=3i4vevrt#s=(m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
